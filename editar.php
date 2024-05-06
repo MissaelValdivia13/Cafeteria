38cdc6da6b98646c3b9509f30b28bb5b96baf13b
Alexa13 <alexalj1013@gmail.com>
2024-05-06 11:41:41 -0600

    Modifique el inicio

--- editar.php
+++ editar.php
@@ -97,7 +97,7 @@
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+                <a class="nav-link active" aria-current="page" href="index.php">Inic</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#producto">Menu</a>
