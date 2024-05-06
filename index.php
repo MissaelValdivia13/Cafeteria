af3cbd1777fa8da8f02dcb3ca3ac54622b398388
allshxn <alohalo9@hotmail.com>
2024-05-06 11:44:38 -0600

    ACTUALICE UwU OwO OuO UnU UuU

--- index.php
+++ index.php
@@ -36,6 +36,9 @@
                   <li class="nav-item">
                     <a class="nav-link" href="registrar.html">Registrar</a>
                   </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="registrar.html">Actualizacion UwU</a>
+                  </li>
               </li>
             </ul>
           </div>
